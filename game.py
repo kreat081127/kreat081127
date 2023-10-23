ab5eab8e199233968083c5e1138503d14580892e
kreat081127 <129774068+kreat081127@users.noreply.github.com>
2023-10-23 19:44:58 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,43 +1,53 @@
-import pygame as py
-py.init()
-font_name = py.font.match_font('Arial') #поиск шрифта
+import pygame as pg
+pg.init()
+font_name = pg.font.match_font('Arial') #поиск шрифта
 size = 18 #его размер
 
 w,h = 600,600
-win = py.display.set_mode((w,h))
+win = pg.display.set_mode((w,h))
 
-image = py.image.load('pngwing.png') #загрузка (добавление) картинки
-image = py.transform.scale(image, (w,h)) #изменение картинки на полный экран
-
+# image = pg.image.load('pngwing.png') #загрузка (добавление) картинки
+# image = pg.transform.scale(image, (w,h)) #изменение картинки на полный экран
 name = ''
 def draw_text(surf, text, x, y, size=size, color=(255,255,255)):
-  font = py.font.Font(font_name, size) #определение шрифта
+  font = pg.font.Font(font_name, size) #определение шрифта
   text_surface = font.render(text, True, color)
   text_rect = text_surface.get_rect()
   text_rect.midtop = (x,y)
   surf.blit(text_surface, text_rect)
 def user_name(surf,text,x,y,size):
-  font = py.font.Font(font_name, size)  # определение шрифта
+  font = pg.font.Font(font_name, size)  # определение шрифта
   text_surface = font.render(text, True, color=(255,255,255))
   text_rect = text_surface.get_rect()
   text_rect.midtop = (x, y)
   surf.blit(text_surface, text_rect)
-fps = py.time.Clock
-while 1:
-  for event in py.event.get():
-    if event.type == py.QUIT:
+fps = pg.time.Clock
+main = True
+while main:
+  for event in pg.event.get():
+    if event.type == pg.QUIT:
       exit()
-    elif event.type == py.KEYDOWN:
-      if event.key == py.K_BACKSPACE:
+    elif event.type == pg.KEYDOWN:
+      if event.key == pg.K_BACKSPACE:
         name = name[:-1]
-      elif event.key == py.K_RETURN:
-        print(name)
-        name = ''
+      elif event.key == pg.K_RETURN:
+        main = False
       else:
         name += event.unicode
   win.fill((0,0,0))
-  win.blit(image, (0, 0)) #добавление картинки в игру
+  # win.blit(image, (0, 0)) #добавление картинки в игру
   draw_text(win, 'Введите имя:', (w//2),(h//2))
   draw_text(win, name, 300,325)
-  py.display.update()
-  py.time.delay(10)
+  pg.display.update()
+
+
+while True:
+  for i in pg.event.get():
+    if i.type == pg.QUIT:
+      exit()
+  win.fill((119,0,255))
+  for y in range(0,h,10):
+    for x in range(0,w,10):
+      pg.draw.line(win,(0,0,0), (0,y),(w,y))
+      pg.draw.line(win, (0,0,0), (x, 0), (x, h))
+  pg.display.update()
